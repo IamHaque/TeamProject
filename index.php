c0984ebb19b5de05aaae3cf34d69ef0c6cbf350f
Subhan Ul Haque <32739861+IamHaque@users.noreply.github.com>
2017-10-13 21:37:32 -0700

    Added login option to index

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="">
     <head>
+       
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
         <title>Mass Bunk Planner</title>
         
 <!--    Font Families    -->
@@ -16,107 +17,53 @@
         <link rel="stylesheet" href="CSS/bootstrap-grid.css" type="text/css">
         <link rel="stylesheet" href="CSS/index.css" type="text/css">
         
-        <style>
-            body {
-              background: #ffa500;
-            }
-            form > div {
-              position: relative;
-              margin-bottom: 10px;
-            }
-            .theTooltip {
-              -webkit-backface-visibility: hidden;
-                      backface-visibility: hidden;
-              will-change: opacity, visibility;
-              max-width: 250px;
-              border-radius: 5px;
-              background-color: rgba(0,0,0,0.7);
-              padding: 15px;
-              color: #fff;
-              box-sizing: border-box;
-              display: inline-block;
-              position: absolute;
-              -webkit-transform-style: preserve-3d;
-                      transform-style: preserve-3d;
-              -webkit-transform: translate(15%, -50%);
-                      transform: translate(15%, -50%);
-              top: 50%;
-              left: auto;
-              right: auto;
-              bottom: auto;
-              visibility: hidden;
-              margin: 0;
-              opacity: 0;
-              -webkit-transition: opacity 0.3s ease-out;
-              transition: opacity 0.3s ease-out;
-              z-index: 100;
-            }
-            .theTooltip:after {
-              content: '';
-              position: absolute;
-              width: 0;
-              height: 0;
-              top: 50%;
-              margin-top: -10px;
-              left: -10px;
-              border-top: 10px solid transparent;
-              border-bottom: 10px solid transparent;
-              border-right: 10px solid rgba(0,0,0,0.7);
-            }
-            label {
-              display: inline-block;
-              vertical-align: center;
-            }
-            input {
-              background: #fff;
-              border: 1px solid transparent;
-              cursor: pointer;
-              display: inline-block;
-              overflow: visible;
-              margin: 0;
-              outline: 0;
-              vertical-align: center;
-              text-decoration: none;
-              width: auto;
-              border-radius: 3px;
-              cursor: text;
-              padding: 7px;
-            }
-            input:focus,
-            input:active {
-              outline: none;
-            }
-            .submitted input:invalid {
-              border: 1px solid #f00;
-            }
-            .submitted input:invalid ~ .theTooltip {
-              visibility: visible;
-              opacity: 1;
-            }
-            .submitted input:valid ~ .theTooltip {
-              -webkit-transition: opacity 0.3s, visibility 0s 0.3s;
-              transition: opacity 0.3s, visibility 0s 0.3s;
-            }
-        </style>
     </head>
 
     <body>
-
-       <form id="frm" action="action">
-          <div>
-            <label>Email</label>
-            <input type="email" required="required"/><span class="theTooltip">Sahi se likho hutiya</span>
-          </div>
-          <div>
-            <button formnovalidate="formnovalidate">OK</button>
-          </div>
-        </form>
+        
+        <div class="header">
+            <div class="loginDiv selected">Login</div>
+            <div class="registerDiv">Register</div>
+        </div>
+        
+        <div class="login">
+            <form action="#" method=post class="form" autocomplete="off">
+                <div class="input-effect">
+                    <input class="effect" type="email" placeholder="" name="email">
+                    <label>Placeholder</label>
+                    <span class="focus-border"></span>
+                </div>
+                <div class="input-effect">
+                    <input class="effect" type="password" placeholder="" name="password">
+                    <label>Placeholder</label>
+                    <span class="focus-border"></span>
+                </div>
+                <input type="submit" value="Login" class="button">
+            </form>
+        </div>
+        <div class="contact">Contact Us</div>
         
         <script src="JS/jquery.min.js"></script>
         <script>
-            document.querySelector('#frm').addEventListener('submit', e => {
-              e.preventDefault();
-              e.currentTarget.classList.add('submitted');
+            $(window).load(function() {
+                var height = $(window).height();
+                var width = $(window).width();
+                $("title").html(width + " X " + height);
+                
+                $(".input-effect input").val("");
+
+                $(".input-effect input").focusout(function() {
+                    if ($(this).val() != "") {
+                        $(this).addClass("has-content");
+                    } else {
+                        $(this).removeClass("has-content");
+                    }
+                });
+            });
+            $(window).resize(function() {
+                var height = $(window).height();
+                var width = $(window).width();
+                $("title").html(width + " X " + height);
             });
         </script>
     </body>
